37154841e7915d83d7b11881a5ad6500e2b01feb
Antoni Rubio Santiago <antoni.rubio@inslapineda.cat>
2023-12-01 09:47:06 +0100

    Pie

--- views/general/pies.php
+++ views/general/pies.php
@@ -4,9 +4,9 @@ echo "<footer>
         <img src = './img/chat.png' alt = 'Chat' class = 'ImgPie'><p>Contacta<br> con nosotros</p>
     </div>
     <div class = 'copy'>
-        <p><strong>© 2023 Cómics House. Todos los derechos reservados.</p>
-        <p>Comics House S.L. - C/ Vietnam, Badalona. 08912</p> 
-        <p>info@comicshouse.com</strong></p>
+        <p><strong>© 2023 Manga House. Todos los derechos reservados.</p>
+        <p>Manga House S.L. - C/ Vietnam, Badalona. 08912</p> 
+        <p>info@mangahouse.com</strong></p>
     </div>
     <div class = 'RRSS'>
         <img src = './img/twitter.png' alt = 'Chat' class = 'ImgPie'>
