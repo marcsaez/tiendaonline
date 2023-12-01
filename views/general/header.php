7b4367f05ac6e31628dbbb76563fd076ddeccc72
Antoni Rubio Santiago <antoni.rubio@inslapineda.cat>
2023-12-01 09:39:15 +0100

    Pie

--- views/general/header.php
+++ views/general/header.php
@@ -1,16 +1,15 @@
 <?php
-echo"<link rel='stylesheet' type='text/css' href='../../css/styles.css'>";
 echo "<header>
     <div class = 'header'>
-    <img src='../../img/Logo.png' alt='MangaHouse' id='logo'>
+    <img src='./img/Logo.png' alt='MangaHouse' id='logo'>
     </div>
     <div id = 'busqueda'>
     <input type='text' placeholder='Indique lo que desea buscar...' id ='navegacion'>
     </div>
     <div id = 'acciones'>
-    <img src='../../img/carrito.png' alt='Carrito' class='imghdr'>
-    <img src='../../img/user.png' alt='Usuario' class='imghdr'>
-    <img src='../../img/desplegable.png' alt='Desplegable' class='imghdr'>
+    <img src='./img/carrito.png' alt='Carrito' class='imghdr'>
+    <img src='./img/user.png' alt='Usuario' class='imghdr'>
+    <img src='./img/desplegable.png' alt='Desplegable' class='imghdr'>
     </div>";
     
 echo "</header>";
---
+++ views/general/pies.php
@@ -0,0 +1,19 @@
+<?php 
+echo "<footer>
+    <div class = 'chat'>
+        <img src = './img/chat.png' alt = 'Chat' class = 'ImgPie'><p>Contacta<br> con nosotros</p>
+    </div>
+    <div class = 'copy'>
+        <p><strong>© 2023 Cómics House. Todos los derechos reservados.</p>
+        <p>Comics House S.L. - C/ Vietnam, Badalona. 08912</p> 
+        <p>info@comicshouse.com</strong></p>
+    </div>
+    <div class = 'RRSS'>
+        <img src = './img/twitter.png' alt = 'Chat' class = 'ImgPie'>
+        <img src = './img/insta.png' alt = 'Chat' class = 'ImgPie'>
+        <img src = './img/facebook.png' alt = 'Chat' class = 'ImgPie'>
+    </div>
+
+";
+echo"</footer>";
+?>
