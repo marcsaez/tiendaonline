28658e66e04606d6a1cb8e364646d3166b6dc5f7
ErikFNGit <erik.fernandez@inslapineda.cat>
2023-12-13 12:45:11 +0100

    algoritmo id producto

---
+++ AlgoritmoIDProducto.php
@@ -0,0 +1,34 @@
+<?php
+function generarCodigoUnico($categoria, $nombre, $numero, $longitudCategoria = 2, $longitudNombre = 2) {
+    $codigoCategoria = substr(strtoupper($categoria), 0, $longitudCategoria);
+    $codigoNumero = str_pad($numero, 3, '0', STR_PAD_LEFT);
+    $codigoNombre = substr(strtoupper($nombre), 0, $longitudNombre);
+    $codigoUnico = $codigoCategoria . $codigoNumero . '-' . $codigoNombre;
+    return $codigoUnico;
+}
+function obtenerSiguienteNumeroProducto() {
+    $conexion = pg_connect("host=localhost dbname=nombre_base_de_datos user=usuario password=contraseña");
+    if (!$conexion) {
+        die("Error de conexión: " . pg_last_error());
+    }
+    $query = "SELECT COUNT(*) as total FROM products";
+    $resultado = pg_query($conexion, $query);
+    if ($resultado) {
+        $fila = pg_fetch_assoc($resultado);
+        $siguienteNumero = $fila['total'] + 1;
+        pg_free_result($resultado);
+        pg_close($conexion);
+        return $siguienteNumero;
+    } else {
+        echo "Error en la consulta: " . pg_last_error($conexion);
+    }
+    pg_close($conexion);  
+    return false;
+}
+    
+$categoria = "Electrónica";
+$nombre = "Smartphone";
+$numero = obtenerSiguienteNumeroProducto();
+
+$codigoGenerado = generarCodigoUnico($categoria, $nombre, $numero);
+echo "Código único generado: " . $codigoGenerado;
