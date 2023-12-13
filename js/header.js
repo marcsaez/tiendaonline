4b71c7a4286072800574b24408281e82e2329371
ErikFNGit <erik.fernandez@inslapineda.cat>
2023-12-13 14:16:09 +0100

    carpeta js con el js de la vista Header

---
+++ js/header.js
@@ -0,0 +1,16 @@
+function mostrarDesplegable() {
+    var desplegable = document.getElementById("desplegable");
+    if (desplegable.style.display === "none" || desplegable.style.display === "") {
+        desplegable.style.display = "block";
+    } else {
+        desplegable.style.display = "none";
+    }
+}
+function mostrarMenu() {
+    var desplegable = document.getElementById("desplegableMenu");
+    if (desplegable.style.display === "none" || desplegable.style.display === "") {
+        desplegable.style.display = "block";
+    } else {er
+        desplegable.style.display = "none";
+    }
+}
