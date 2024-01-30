f31081885c175e126ae19a9211f23961f6d4c80c
Antoni Rubio Santiago <antoni.rubio@inslapineda.cat>
2024-01-30 14:03:39 +0100

    Canvas firma

---
+++ docker/apache/app/views/admin/guardar_imagen_canvas.php
@@ -0,0 +1,16 @@
+<?php
+// Obtener la imagen codificada desde la solicitud POST
+$imagen_codificada = $_POST['imagen'];
+echo "$imagen_codificada";
+
+// Decodificar la imagen base64
+$imagen_decodificada = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $imagen_codificada));
+
+// Ruta donde deseas guardar la imagen (NOMBRE SERA LA ID DE LA FACTURA)
+$ruta_guardado = '../../img/firmas/' . 'imagen_canvas.png';
+
+// Guardar la imagen en el servidor
+file_put_contents($ruta_guardado, $imagen_decodificada);
+
+echo 'Imagen guardada correctamente en ' . $ruta_guardado;
+?>
