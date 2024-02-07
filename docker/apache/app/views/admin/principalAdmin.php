77568dba5209db00a4517a4d16f914389b399ac6
Antoni Rubio Santiago <antoni.rubio@inslapineda.cat>
2024-02-07 12:27:10 +0100

    Grafico en js

--- docker/apache/app/views/admin/principalAdmin.php
+++ docker/apache/app/views/admin/principalAdmin.php
@@ -4,41 +4,13 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/4.0.0/fabric.min.js"></script>
   <div id="chartContainer" style="height: 370px; width: 100%;"></div>
   <script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
+  <script>
+        // Incluir los datos JSON directamente en una variable JavaScript
+        var datosJson = <?php echo json_encode($datos); ?>;
+    </script>
 </main>
 
 
-<script>
-window.onload = function () {
-
-var chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,
-	title:{
-		text: "Productos por categoría",
-		horizontalAlign: "left"
-	},
-	data: [{
-		type: "doughnut",
-		startAngle: 60,
-		//innerRadius: 60,
-		indexLabelFontSize: 17,
-		indexLabel: "{label} - #percent%",
-		toolTipContent: "<b>{label}:</b> {y} (#percent%)",
-    dataPoints: [
-		
-    <?php
-        if (isset($datos)) {
-            foreach ($datos as $dato) {
-                $contador = $dato['y'];
-                $nombreCategoria = $dato['label'];
-                
-                echo "{ y: $contador, label: '$nombreCategoria' },";
-            }
-        }
-        ?> 
-    ]
-	}]
-});
-chart.render();
-
-}
-</script>
+<div id="Grafico1">
+<script src="./js/canvasCirculo.js"></script>
+</div>
