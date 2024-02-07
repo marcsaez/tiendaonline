dad84eee56a6d334a84e99a694055a33609177a1
MarinaLl <llambrichdev@gmail.com>
2024-02-07 13:13:38 +0100

    no se

--- docker/apache/app/views/general/carrito.php
+++ docker/apache/app/views/general/carrito.php
@@ -1,12 +1,15 @@
-<div class="miCarrito">
+<!-- <div class="miCarrito">
     <h2>Mi carrito</h2>
-    <!-- aqui se mostraran los productos generados mediante js para poder aumentar y disminuir la cantidad de forma dinamica-->
-</div>
-<div class="resuemn">
+</div> -->
+<!-- aqui se mostraran los productos generados mediante js para poder aumentar y disminuir la cantidad de forma dinamica-->
+<!-- Lista generada de productos con cantidad + precio en orden "CantidadXNombrePrecio" -->
+<!-- <div class="resuemn">
     <h2>Resumen</h2>
     <ul>
-        <!-- Lista generada de productos con cantidad + precio en orden "CantidadXNombrePrecio" -->
     </ul>
     <p>Total : <span id="id">0</span></p>
-    <p><?php echo $_SESSION['carrito']; ?></p>
-</div>
+    <p><?php // echo $_SESSION['carrito']; ?></p>
+</div> -->
+<main class="page-grid">
+    <h1 class="page-title">Mi Carrito (X)</h1>
+</main>
