de2844ace42e9e75550d2b7aee56526d1b513cf5
MarinaLl <llambrichdev@gmail.com>
2024-02-07 13:24:34 +0100

    session start

--- docker/apache/app/views/general/carrito.php
+++ docker/apache/app/views/general/carrito.php
@@ -1,7 +1,3 @@
-<?php
-session_start();
-?>
-
 <!-- aqui se mostraran los productos generados mediante js para poder aumentar y disminuir la cantidad de forma dinamica-->
 <!-- Lista generada de productos con cantidad + precio en orden "CantidadXNombrePrecio" -->
 <!-- <div class="resuemn">
@@ -9,8 +5,11 @@ session_start();
     <ul>
     </ul>
     <p>Total : <span id="id">0</span></p>
-    <p><?php // echo $_SESSION['carrito']; ?></p>
+    <p>></p>
 </div> -->
 <main class="page-grid">
     <h1 class="page-title">Mi Carrito (X)</h1>
+    <p>
+        <?php print_r($_SESSION); ?>
+    </p>
 </main>
