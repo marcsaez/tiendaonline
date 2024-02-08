bf6c0cfd3a2d39f3a254227252962fa48c6de575
MarinaLl <llambrichdev@gmail.com>
2024-02-08 19:04:57 +0100

    arreglos en el footer

--- docker/apache/app/models/database.php
+++ docker/apache/app/models/database.php
@@ -1,27 +1,27 @@
 <?php
 class database{
     public function conectar(){
-        $host = 'postgres';
+        $host = 'localhost';
         $dbname = 'mangahouse';
-        $user = 'postgresql';
-        $password = 'password';
+        $user = 'postgres';
+        $password = 'root';
 
 
-            $this->db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
-            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $this->db;    
+        $this->db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $this->db;    
     }
 
     public static function staticConectar(){
-        $host = 'postgres';
+        $host = 'localhost';
         $dbname = 'mangahouse';
-        $user = 'postgresql';
-        $password = 'password';
+        $user = 'postgres';
+        $password = 'root';
 
 
-            $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $db;    
+        $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $db;    
     }
 }
 
