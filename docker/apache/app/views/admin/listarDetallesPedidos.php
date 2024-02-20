666b4afc02930339d83957644545d1e1e17fa6af
Antoni Rubio Santiago <antoni.rubio@inslapineda.cat>
2024-02-20 12:26:00 +0100

    Detalles solucionado

--- docker/apache/app/views/admin/listarDetallesPedidos.php
+++ docker/apache/app/views/admin/listarDetallesPedidos.php
@@ -81,5 +81,74 @@
             </div>
         </main>
         <?php
-    }
+    } else{
+        ?>
+        <main class="dashboard">
+            <div class="admin-content">
+                <section class="admin-headers">
+                    <h1>Detalle Pedido</h1>
+                </section>
+                <section>
+                    <div id="customer-email" data-email="<?php echo $email; ?>"></div>
+                    <div id="factura">
+                        <fieldset>
+                            <legend>Datos Cliente</legend>
+                            <section id="datos-usuario">
+                                <div>
+                                    Nombre:
+                                </div>
+                                <div>
+                                    Email:
+                                </div>
+                                <div>
+                                    Telefono:
+                                </div>
+                                <div>
+                                    Direccion:
+                                </div>
+                                <div>
+                                    <?php echo "Sin Datos por el momento" ?>
+                                </div>
+                                <div>
+                                <?php echo "Sin Datos por el momento" ?>
+                                </div>
+                                <div>
+                                <?php echo "Sin Datos por el momento" ?>
+                                </div>
+                                <div>
+                                <?php echo "Sin Datos por el momento" ?>
+                                </div>
+                            </section>
+                        </fieldset>
+                      
+                        <table class="admin-table">
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Cantidad</th>
+                                <th>Precio Total</th>
+                            </tr>
+                            <?php
+                            $totalPriceSum = 0;
+                            ?>
+                            <tr>
+                                <td><?php echo "Sin Datos por el momento" ?></td>
+                                <td><?php echo "Sin Datos por el momento" ?></td>
+                                <td><?php echo "Sin Datos por el momento" ?></td>
+                                <td><?php echo "0€" ?></td>
+                            </tr>
+                            <tr>
+                                <td colspan="3">Total</td>
+                                <td><?php echo $totalPriceSum ?>€</td>
+                            </tr>
+                            <tr>
+                                <!-- COLOR LILA SIEMPRE Y EN EL CENTRO PORFAVOR !--
+                            </tr>
+                        </table>
+                    </div>      
+                </section>
+            </div>
+        </main>
+        <?php }
+    
 ?>
